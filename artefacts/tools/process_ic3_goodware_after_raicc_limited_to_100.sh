0877162a63c078043fb94cbfd925cf6a831ca215
Jordan Samhi <j.samhi@me.com>
2021-01-14 17:57:20 +0100

    Add: minimal dataset

--- artefacts/tools/process_ic3_goodware_after_raicc_limited_to_100.sh
+++ artefacts/tools/process_ic3_goodware_after_raicc_limited_to_100.sh
@@ -1,4 +1,3 @@
-i=0
 while read sha
 do
 	echo "Downloading $sha..."
@@ -10,9 +9,4 @@ do
 		./launch_ic3.sh -a raicc_output/$sha.apk -p ~/git/android-platforms/ -d cc
 		rm raicc_output/$sha.apk
 	fi
-	let "i+=1"
-	if [ $i -eq 100 ]
-	then
-		break
-	fi
-done < /home/raicc/git/RAICC/artefacts/hashes/5k_ic3_raicc_benign_section_V_D_1.txt
+done < /home/raicc/git/RAICC/artefacts/hashes/minimal_datasets/ic3_benign_100.txt
